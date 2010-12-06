66bae42466cc71111fab9d146a737ed2a985a62d
Erdbeere <erd_baer@t-online.de>
2010-12-06 23:12:59 +0100

    Removed some debug stuff.

--- teemap.py
+++ teemap.py
@@ -102,11 +102,7 @@ class Teemap(object):
                         getattr(self, name).append(_class(item))
                     elif item.type == _type:
                         _class = getattr(items, _type.title())
-                        getattr(self, name).append(_class(item))
-            print self.groups
-            print self.layers
-            items.Quad(self.itemlist[-2])
-            #for item in self.items:
+                        getattr(self, name).append(_class(item))#for item in self.items:
             #    # load groups
             #    if item.type == 'group':
             #        group = items.Group(len(self.groups)+1, item.data[2:])
