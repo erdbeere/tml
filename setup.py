1f8144ffdecbf347fbb84ef0a80e2849cca3a386
Krzysztof Socha <ksocha@ksocha.com>
2010-12-29 03:57:26 +0100

    rearranged repository, I hope it works

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('tml').__doc__.splitlines()).strip()
+
+setup(
+    name = "tml",
+    version = __import__('tml').get_version().replace(' ', '-'),
+    url = '',
+    author = '',
+    author_email = '',
+    description = DESC,
+    long_description = read_file('README'),
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires=read_file('requirements.txt'),
+    classifiers = [
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+    ],
+)
