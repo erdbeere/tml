9ad002eba41e280a1159cc8c40dcbcfd82099062
erdbeere <erd_baer@t-online.de>
2011-07-19 17:28:47 +0200

    Less redundancy in TestTeemap
    Use setUp() to create an instance of a Teemap instead of creating one in each
    test.

--- tml/test_tml.py
+++ tml/test_tml.py
@@ -8,11 +8,11 @@ from items import Layer, QuadLayer, TileLayer
 class TestHeader(unittest.TestCase):
 
     def setUp(self):
-        pass
+        with open('tml/maps/dm1.map') as f:
+            self.header = Header(f)
 
     def test_signature(self):
-        with open('tml/maps/dm1.map') as f:
-            header = Header(f)
+        pass
 
     def test_load(self):
         assert Teemap('tml/maps/dm1.map')
@@ -25,36 +25,34 @@ class TestHeader(unittest.TestCase):
 class TestTeemap(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.teemap = Teemap('tml/test_maps/vanilla')
 
     def test_groups(self):
-        t = Teemap('tml/test_maps/vanilla')
-        self.assertEqual(len(t.groups), 7)
+        self.assertEqual(len(self.teemap.groups), 7)
         names = [None, None, 'Game', 'NamedGroup', None, None, 'OtherGroup']
-        for i, group in enumerate(t.groups):
+        for i, group in enumerate(self.teemap.groups):
             self.assertEqual(group.name, names[i])
 
     def test_layers(self):
-        t = Teemap('tml/test_maps/vanilla')
-        self.assertEqual(len(t.layers), 6)
+        self.assertEqual(len(self.teemap.layers), 6)
         for i, num in enumerate([1,2,1,0,2,0,0]):
-            self.assertEqual(len(t.groups[i].layers), num)
+            self.assertEqual(len(self.teemap.groups[i].layers), num)
 
-        self.assertIs(t.groups[0].layers[0], t.layers[0])
-        self.assertIs(t.groups[1].layers[0], t.layers[1])
-        self.assertIs(t.groups[1].layers[1], t.layers[2])
-        self.assertIs(t.groups[2].layers[0], t.layers[3])
-        self.assertIs(t.groups[4].layers[0], t.layers[4])
-        self.assertIs(t.groups[4].layers[1], t.layers[5])
+        self.assertIs(self.teemap.groups[0].layers[0], self.teemap.layers[0])
+        self.assertIs(self.teemap.groups[1].layers[0], self.teemap.layers[1])
+        self.assertIs(self.teemap.groups[1].layers[1], self.teemap.layers[2])
+        self.assertIs(self.teemap.groups[2].layers[0], self.teemap.layers[3])
+        self.assertIs(self.teemap.groups[4].layers[0], self.teemap.layers[4])
+        self.assertIs(self.teemap.groups[4].layers[1], self.teemap.layers[5])
 
         names = ['TestQuads', 'Quads', 'TestTiles', 'Game', None, 'LastTiles']
         classes = [QuadLayer, QuadLayer, TileLayer, TileLayer, TileLayer,
                    TileLayer]
-        for i, layer in enumerate(t.layers):
+        for i, layer in enumerate(self.teemap.layers):
             self.assertIsInstance(layer, classes[i])
             self.assertEqual(layer.name, names[i])
 
-        for layer in t.layers:
+        for layer in self.teemap.layers:
             if isinstance(layer, TileLayer):
                 if layer.name == 'TestTiles':
                     self.assertEqual(layer.width, 42)
@@ -64,21 +62,19 @@ class TestTeemap(unittest.TestCase):
                     self.assertEqual(layer.height, 50)
 
     def test_envelopes(self):
-        t = Teemap('tml/test_maps/vanilla')
-        self.assertEqual(len(t.envelopes), 2)
-        self.assertEqual(t.envelopes[0].name, 'PosEnv')
-        self.assertEqual(t.envelopes[1].name, 'ColorEnv')
+        self.assertEqual(len(self.teemap.envelopes), 2)
+        self.assertEqual(self.teemap.envelopes[0].name, 'PosEnv')
+        self.assertEqual(self.teemap.envelopes[1].name, 'ColorEnv')
 
     def test_envpoints(self):
-        t = Teemap('tml/test_maps/vanilla')
-        self.assertEqual(len(t.envpoints), 9)
-        self.assertEqual(len(t.envelopes[0].envpoints), 4)
-        self.assertEqual(len(t.envelopes[1].envpoints), 5)
+        self.assertEqual(len(self.teemap.envpoints), 9)
+        self.assertEqual(len(self.teemap.envelopes[0].envpoints), 4)
+        self.assertEqual(len(self.teemap.envelopes[1].envpoints), 5)
 
-        for i, envpoint in enumerate(t.envelopes[0].envpoints):
-            self.assertIs(envpoint, t.envpoints[i])
-        for i, envpoint in enumerate(t.envelopes[1].envpoints):
-            self.assertIs(envpoint, t.envpoints[i+4])
+        for i, envpoint in enumerate(self.teemap.envelopes[0].envpoints):
+            self.assertIs(envpoint, self.teemap.envpoints[i])
+        for i, envpoint in enumerate(self.teemap.envelopes[1].envpoints):
+            self.assertIs(envpoint, self.teemap.envpoints[i+4])
 
 if __name__ == '__main__':
     unittest.main()
