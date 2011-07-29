ee3c9bd6e769aed1b120dcc3da99f358ad557d56
erdbeere <erd_baer@t-online.de>
2011-07-29 13:11:50 +0200

    Test for utils.

---
+++ tml/test_utils.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from utils import ints_to_string, string_to_ints
+
+TEST_INT = [4108710900L, 2155905152L, 2155905152L, 2155905152L, 2155905152L,
+            2155905152L, 2155905152L, 2155905024L]
+class TestUtils(unittest.TestCase):
+
+    def test_ints_to_string(self):
+        test = ints_to_string(TEST_INT)
+        self.assertEqual(test, 'test')
+
+    def test_string_to_ints(self):
+        test = string_to_ints('test')
+        self.assertEqual(test, TEST_INT)
